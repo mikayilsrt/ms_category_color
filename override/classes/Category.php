9b47160cd074b63ffdbee350c569a62702784e0a
mikayilsrt <sert.mikayil@gmail.com>
2020-01-09 16:22:39 +0100

    feat: override category classe

---
+++ override/classes/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+class Category extends CategoryCore
+{
+
+    public $category_color;
+
+    public function __construct($idCategory = null, $idLang = null, $idShop = null)
+    {
+        self::$definition['fields']['category_color'] = array(
+            'type'  =>  self::TYPE_STRING,
+            'validate'  =>  'isGenericName',
+            'size'  =>  255,
+        );
+
+        parent::__construct($idCategory, $idLang, $idShop);
+    }
+
+}
